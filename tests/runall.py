021000669d1f8ce3a66fb82b8803cf13a462afb7
James Jones <james.jones.jmj@gmail.com>
2012-10-30 16:35:56 +0000

    runall.py: executable can now be set on cmd line.
    If the executable is not set on the command line then more paths will be
    searched than it would have done previously.

--- tests/runall.py
+++ tests/runall.py
@@ -1,8 +1,10 @@
 from subprocess import call
 import os;
 import glob;
+import sys;
+import argparse
 
-executable = "../bin/Debug/HLSLByteCodeToGLSLStandalone"
+executable = ""
 
 passCount = 0
 failCount = 0
@@ -10,11 +12,36 @@ listOfFailedTests = []
 
 i = 0
 
+parser = argparse.ArgumentParser(description='Test runner.')
+parser.add_argument("-e", "--exe", dest="argExecutableName", required=False, help="The executable")
+options = parser.parse_args()
+
+def FindExecutable():
+	global executable
+
+	if os.path.isfile("../bin/HLSLByteCodeToGLSLStandalone.exe") or os.path.isfile("../bin/HLSLByteCodeToGLSLStandalone"):
+		executable = "../bin//HLSLByteCodeToGLSLStandalone"
+	elif os.path.isfile("../bin/Release/HLSLByteCodeToGLSLStandalone.exe") or os.path.isfile("../bin/Release/HLSLByteCodeToGLSLStandalone"):
+		executable = "../bin/Release/HLSLByteCodeToGLSLStandalone"
+	elif os.path.isfile("../bin/Debug/HLSLByteCodeToGLSLStandalone.exe") or os.path.isfile("../bin/Debug/HLSLByteCodeToGLSLStandalone"):
+		executable = "../bin/Debug/HLSLByteCodeToGLSLStandalone"
+	else:
+		#Search system paths
+		for path in os.environ["PATH"].split(os.pathsep):
+			win_exe_file = os.path.join(path, "HLSLByteCodeToGLSLStandalone.exe")
+			otheros_exe_file = os.path.join(path, "HLSLByteCodeToGLSLStandalone")
+			if os.path.isfile(win_exe_file) or os.path.isfile(otheros_exe_file):
+				executable = otheros_exe_file #okay on windows as well.
+				return
+		print "Failed to find exe.\n"
+		sys.exit(0)
+
 def RunTest(ByteCodeFileName, lang):
 
 	global failCount
 	global passCount
 	global listOfFailedTests
+	global executable
 
 	(head, tail) = os.path.split(ByteCodeFileName);
 	outputfilename = tail + ".glsl"
@@ -31,6 +58,11 @@ def RunTest(ByteCodeFileName, lang):
 		listOfFailedTests.append(ByteCodeFileName)
 	else:
 		passCount += 1
+
+if(options.argExecutableName):
+	executable = options.argExecutableName
+else:
+	FindExecutable()
 
 for files in glob.glob("vs4/*.o"):
 	RunTest(files, "150")
