d8eeada947cf2291f132fae7ee07311d4f63ae0e
James Jones <james.jones.jmj@gmail.com>
2013-01-30 12:22:41 +0000

    Use a cube instead of a quad in tessellation test.

--- tests/apps/tessellation/main.cpp
+++ tests/apps/tessellation/main.cpp
@@ -50,7 +50,7 @@ void display(void)
         dwTimeStart = dwTimeCur;
     t = ( dwTimeCur - dwTimeStart ) / 1000.0f;
 
-    gWorld = Matrix4::identity(); //Matrix4::rotationY(t);
+    gWorld = Matrix4::rotationY(t);
 
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -58,8 +58,8 @@ void display(void)
 
     mTessEffect.Enable();
 
-    gGlobals.InnerFactor = 4;
-    gGlobals.OuterFactor = 4;
+    gGlobals.InnerFactor = 1+t/2;
+    gGlobals.OuterFactor = 1+t/2;
 
     SetFloatArray(gWorld, gGlobals.World);
     SetFloatArray(gView, gGlobals.View);
@@ -72,11 +72,8 @@ void display(void)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(SimpleVertex), 0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(SimpleVertex), (void*)(sizeof(float)*3));
 
-    //glDrawElements(GL_PATCHES, 36, GL_UNSIGNED_INT, 0);
-
     glPatchParameteri(GL_PATCH_VERTICES, 4);
-    glDrawArrays (GL_PATCHES, 0, 4);
-
+    glDrawElements(GL_PATCHES, 24, GL_UNSIGNED_INT, 0);
     glutSwapBuffers();
 }
 void reshape (int w, int h)
@@ -119,10 +116,6 @@ void Init(int argc, char** argv)
     SetupOpenGLDebugCallback();
 #endif
 
-    //glFrontFace(GL_CW);
-    //glCullFace(GL_BACK);
-    //glEnable(GL_CULL_FACE);
-
     glClearColor(0.0f, 0.125f, 0.3f, 1.0f);
 
     GLuint vao;
@@ -150,69 +143,23 @@ void Init(int argc, char** argv)
     gView = Matrix4::lookAt(Eye, At, Up);
     gProjection = Matrix4::perspective(3.14159f * 0.25f, WindowWidth / ( float )WindowHeight, 0.1f, 100.0f);
 
-
-	SimpleVertex vertices[] =
-    {
-        {{-0.5,-0.5, 0}, {1, 0, 0}},
-        {{0.5,-0.5, 0}, {1, 0, 0}},
-        {{0.5,0.5, 0}, {1, 0, 0}},
-        {{-0.5,0.5, 0}, {1, 0, 0}},
-     };
-
-	/*static GLfloat colors[] = {0.66f, 0.66f, 0.66f,0.0f,
-							  0.33f, 0.33f, 0.33f,0.0f,
-							  1.0f, 1.0f, 1.0f,0.0f,
-							  1.0f, 0.4f, 0.4f,0.0f,};*/
-
-    /*uint32_t indices[] =
-    {
-        3,1,0,
-        2,1,3,
-
-        6,4,5,
-        7,4,6,
-
-        11,9,8,
-        10,9,11,
-
-        14,12,13,
-        15,12,14,
-
-        19,17,16,
-        18,17,19,
-
-        22,20,21,
-        23,20,22
-    };*/
-
     uint32_t indices[] =
     {
-        0,1,3,
-        3,1,2,
-
-        5,4,6,
-        6,4,7,
-
-        8,9,11,
-        11,9,10,
-
-        13,12,14,
-        14,12,15,
-
-        16,17,19,
-        19,17,18,
-
-        21,20,22,
-        22,20,23
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15,
+        16, 17, 18, 19,
+        20, 21, 22, 23,
     };
 
     glGenBuffers(1, &gIndexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * 36,
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * 24,
         indices, GL_STATIC_DRAW);
 
 
-    /*SimpleVertex vertices[] =
+    SimpleVertex vertices[] =
     {
         { { -1.0f, 1.0f, -1.0f }, { 0.0f, 1.0f, 0.0f } },
         { { 1.0f, 1.0f, -1.0f }, { 0.0f, 1.0f, 0.0f } },
@@ -243,11 +190,11 @@ void Init(int argc, char** argv)
         { { 1.0f, -1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f } },
         { { 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f } },
         { { -1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f } },
-    };*/
+    };
 
     glGenBuffers(1, &gVertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, gVertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(SimpleVertex) * 4,//24,
+    glBufferData(GL_ARRAY_BUFFER, sizeof(SimpleVertex) * 24,
         vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(SimpleVertex), 0);
