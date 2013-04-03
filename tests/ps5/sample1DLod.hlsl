b8bd9a236ba8eca5774d0ca843b26592d3ba6249
James Jones <james.jones.jmj@gmail.com>
2013-04-03 16:44:51 +0100

    Add bias, gradient and offset texture sampling tests.

--- tests/ps5/sample1DLod.hlsl
+++ tests/ps5/sample1DLod.hlsl
@@ -18,7 +18,7 @@ float LodToSample;
 float4 main( PS_INPUT input ) : SV_Target
 {
     float4 base = TextureBase.SampleLevel(TextureSampler, input.TexC, LodToSample);
-    float4 detail = TextureDetail.SampleLevel(TextureSampler, input.TexC, LodToSample);
+    float4 detail = TextureDetail.SampleLevel(TextureSampler, input.TexC, LodToSample, 1);
     return base * detail;
 }
 
