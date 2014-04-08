99f79f9afdc6bb3ffdd03e9b22a280b60e8e19fd
Johan Postma <johan_amtsop@hotmail.com>
2014-04-08 11:45:45 +0200

    fixed a little bug for linux by disabling bsafe string, because gdb couldn't launch

--- src/toGLSLOperand.c
+++ src/toGLSLOperand.c
@@ -995,7 +995,6 @@ static void TranslateVariableName(HLSLCrossCompilerContext* psContext, const Ope
 				else // We don't have a semantic for this variable, so try the raw dump appoach.
 				{
 					bformata(glsl, "cb%d[%d]", psOperand->aui32ArraySizes[0],psOperand->aui32ArraySizes[1]);
-					__nop();
 				}
 
 				//Dx9 only?
