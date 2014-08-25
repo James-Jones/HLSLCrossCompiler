416817894b708476204dd8c801c7a81628ad7bed
Mikko Strandborg <mikko@unity3d.com>
2014-08-25 12:02:52 +0300

    Apple&GNU compilation fixes.

--- src/internal_includes/hlslcc_malloc.c
+++ src/internal_includes/hlslcc_malloc.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+
+#ifdef __APPLE_CC__
+    #include <malloc/malloc.h>
+#else
 #include <malloc.h>
+#endif
 
 void* (*hlslcc_malloc)(size_t size) = malloc;
 void* (*hlslcc_calloc)(size_t num,size_t size) = calloc;
