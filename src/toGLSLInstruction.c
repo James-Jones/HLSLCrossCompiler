8160436e2e3c82928757372ed33f2fc10efccf13
MrShoor <MrShoor@yandex.ru>
2015-04-08 14:32:58 -0700

    Fix incorrect swizzling for ftoi instruction. For example instruction: ftoi r0.yz, r0.yyzy Will translate into: Temp[0].yz = intBitsToFloat(ivec4(Temp[0].yyzy).xy); Instead correct translate: Temp[0].yz = intBitsToFloat(ivec4(Temp[0].yyzy).yz);

--- src/toGLSLInstruction.c
+++ src/toGLSLInstruction.c
@@ -2683,15 +2683,16 @@ void TranslateInstruction(HLSLCrossCompilerContext* psContext, Instruction* psIn
 
 		AddIndentation(psContext);
 
-		AddAssignToDest(psContext, &psInst->asOperands[0], psInst->eOpcode == OPCODE_FTOU ? SVT_UINT : SVT_INT, srcCount, &numParenthesis);
+		AddAssignToDest(psContext, &psInst->asOperands[0], psInst->eOpcode == OPCODE_FTOU ? SVT_UINT : SVT_INT, srcCount, &numParenthesis);		
 		bcatcstr(glsl, GetConstructorForType(psInst->eOpcode == OPCODE_FTOU ? SVT_UINT : SVT_INT, srcCount == dstCount ? dstCount : 4));
 		bcatcstr(glsl, "("); // 1
 		TranslateOperand(psContext, &psInst->asOperands[1], TO_AUTO_BITCAST_TO_FLOAT);
 		bcatcstr(glsl, ")"); // 1
 		// Add destination writemask if the component counts do not match
 		if (srcCount != dstCount)
-			AddSwizzleUsingElementCount(psContext, dstCount);
+			TranslateOperandSwizzleWithMask(psContext, &psInst->asOperands[0], OPERAND_4_COMPONENT_MASK_ALL);
 		AddAssignPrologue(psContext, numParenthesis);
+		//*/
 		break;
 	}
 
